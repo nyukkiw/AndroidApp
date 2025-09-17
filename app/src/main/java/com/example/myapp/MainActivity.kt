c5a1726fa09a8a6bbecec0e03f65d45094d03f4f
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2025-09-17 11:17:56 +0700

    Study about input text,scroll, and show message

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -7,49 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AlertDialog
 class MainActivity : AppCompatActivity() {
-//    berikut adalah tipe dan variable data
-//    jika ingin menulis tipe atau variable data harus diawali dengan
-//    val kemudian dilanjutkan dengan nama variablenya baru titik dua
-//    dan barulah tipe datanya kemudian baru sama dengan terus isinya
-
-//    bedanya val dan var. jika ingin bisa mengubah didalam variable
-//    pakai var dan jika konstanta pakai val
-
-
-//    null. jika ingin buat null harus ada tanda tanyanya apapun tipenya
-
-    val bilanganNull: Int? = null
-
-//    angka
-    var angkaBulat: Int = 10
-
-    val angkaDesimal: Double = 12.5
-    val angkaPanjang: Long=900000000000000L
-// karakter atau kalimat
-    val karakter: Char = 'A'
-    var kata: String = "ini adalah contoh teks panjang" +
-            " enter"
-
-//    logika boolean
-    var benar: Boolean = true
-    val salah: Boolean = false
-
-//    array. untuk diarrah harus ada <> kemudian diisi tipe data didalam
-//    kurung yang berbentuk panah tersebut. kemudian cara isinya dengan
-//    arrayOf untuk mengisinya.
-    val angkaArray: Array<Int> = arrayOf(1,2,3,4,5)
-    val stringArray: Array<String> = arrayOf("aku"," adalah ", "programmer")
-    val booleanArray: Array<Boolean> = arrayOf(true, false, true)
-
-// function didalam kotlin
-    fun hitung(angka: Int, kata: String){
-
-    }
-
-    fun hitung(){
-        angkaBulat++
-    }
 
 
 
@@ -65,24 +28,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val mainLayout: ConstraintLayout = findViewById(R.id.main)
+        val editText: EditText = findViewById(R.id.edit_text)
+        val btnClick: Button = findViewById(R.id.btn_hitung)
 
-        val textView: TextView = findViewById(R.id.text_view)
-        val btnHitung: Button = findViewById(R.id.btn_hitung)
+        btnClick.setOnClickListener {
+            val teks: String = editText.text.toString()
+//            Snackbar.make(mainLayout, teks, Snackbar.LENGTH_SHORT).show()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("myapp")
+            builder.setMessage(teks)
+            builder.setPositiveButton("tutup"){
+                dialog, which ->
+                dialog.dismiss()
+            }
 
-        btnHitung.setOnClickListener {
-            hitung()
-            textView.text = angkaBulat.toString()
+            val dialog = builder.create()
+            dialog.show()
         }
-
-
-
-//        //    if dan elsenya
-//        if(angkaBulat == 4 && angkaDesimal>13){
-//            benar = true
-//            kata = "aku adalah mahasiswa"
-//        }else{
-//            benar = false
-//        }
-
     }
 }
